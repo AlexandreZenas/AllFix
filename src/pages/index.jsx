54cbd0662493c3c4dddf42873e0510f530bd7c98
alexandre zenas <alexandrezenas11@gmail.com>
2025-03-05 16:39:31 -0300

    :tada: Commit inicial

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+
+export function Home() {
+    return (
+        <div>
+            <h1>Welcome to AllFix</h1>
+            <p>Your one-stop solution for all your fixing needs.</p>
+        </div>
+    );
+};
