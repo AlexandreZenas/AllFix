268338f545ad88b99624e4587e19c1856fc173ce
alexandre zenas <alexandrezenas11@gmail.com>
2025-03-10 18:11:55 -0300

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from "./pages"
+import { Navbar } from './components/navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes >
         <Route path="/" element={<Home />} /> 
       </Routes>
