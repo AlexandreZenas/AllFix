219027caf9aee6ccb51ade52c3961f31b0ca92ae
alexandre zenas <alexandrezenas11@gmail.com>
2025-03-11 19:52:26 -0300

    feat: services section

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import { Benefits } from "./benefits";
 import { Hero } from "./hero";
+import { Services } from "./services";
 
 export function Home() {
     return (
         <main>
             <Hero />
             <Benefits />
+            <Services />
         </main>
     );
 };
