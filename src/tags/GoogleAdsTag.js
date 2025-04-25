19bd6c0a8262aa6fd5a6b5ff080dfec651f6dd2b
alexandre zenas <alexandrezenas11@gmail.com>
2025-04-25 17:36:07 -0300

    feat: alter tag ads

--- src/tags/GoogleAdsTag.js
+++
@@ -1,7 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16999866831"></script>
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'AW-16999866831');
